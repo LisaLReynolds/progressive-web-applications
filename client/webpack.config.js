0afe4d39324f268e8ffe243c9a4491394c6b015d
LisaLReynolds <lisalreynolds2015@yahoo.com>
2024-08-02 10:07:26 -0600

    updates to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,20 +1,21 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin"); //create our index.html in our dist bundle
-const WebpackPwaManifest = require("webpack-pwa-manifest"); //create our manifest.json file in our bundle
-const path = require("path");
-const { InjectManifest } = require("workbox-webpack-plugin"); //create our service worker
+const HtmlWebpackPlugin = require('html-webpack-plugin');  //create our index.html in our dist bundle
+const WebpackPwaManifest = require('webpack-pwa-manifest');//create our manifest.json file in our bundle
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');//create our service worker
 
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: "development",
+    mode: 'development',
     entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js",
+      main: './src/js/index.js',
+      install: './src/js/install.js'
     },
     output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
       new HtmlWebpackPlugin({
@@ -22,29 +23,32 @@ module.exports = () => {
         title: "J.A.T.E",
       }),
       new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+        
       }),
       new WebpackPwaManifest({
-        name: "Just Another Text Editor",
-        short_name: "JATE",
-        description: "Text editor that saves when offline",
-        background_color: "#ffffff",
-        start_url: "/",
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'text editor that saves even when offline',
+        background_color: '#ffffff',
+        start_url: '/',
         publicPath: "/",
-        fingerprints: false,
-        inject: true,
+        fingerprints: false, 
+        inject: true,  
         icons: [
           {
-            src: path.resolve("src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"),
+      src: path.resolve('src/images/logo.png'),
+      sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+      destination: path.join("assets", "icons"),
           },
-        ],
-      }),
-    ],
+          {
 
-    // rules for CSS and babel
+          }
+        ]
+      })
+    ],
+// rules for css and babel
     module: {
       rules: [
         {
@@ -55,12 +59,14 @@ module.exports = () => {
           test: /\.(?:js|mjs|cjs)$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"],
-            },
-          },
-        },
+              presets: [
+                ['@babel/preset-env', { targets: "defaults" }]
+              ]
+            }
+          }
+        }
       ],
     },
   };
