8f696c0dd688ae7e72ba10b0c2cd79fcd6224439
LisaLReynolds <lisalreynolds2015@yahoo.com>
2024-08-02 09:47:56 -0600

    updates

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,51 +1,45 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  //initDB
-  //we have to make a connection to db
-  const dbJate = await openDB('jate', 1);
-  //open up a transaction with an grant permissions of read/write
-  const trans = dbJate.transaction('jate', "readwrite");
-  //then connect to object store
-
-  const jateStore = trans.objectStore('jate');
-
-  const request = jateStore.put({id: 1, value: content });
+  // initDB
+  //1.make connection to db
+  const dbJate = await openDB("jate", 1);
+  //2. open up a transaction with an grant permission of read/write
+  const trans = dbJate.transaction("jate", "readwrite");
+  const jateStore = trans.objectStore("jate");
+  const request = jateStore.put({ id: 1, value: content });
 
   const response = await request;
   console.log(response);
-  
-  //then we can call whatever
-}
+
+  //3. then going to connect to object store
+  //4. then call whatever
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-    //we have to make a connection to db
-    const dbJate = await openDB('jate', 1);
-    //open up a transaction with an grant permissions of read/write
-    const trans = dbJate.transaction('jate', "read");
-    //then connect to object store
-  
-    const jateStore = trans.objectStore('jate');
-  
-    const request = jateStore.get(1);
-  
-    const response = await request;
-    console.log(response);
-    
-}
+  const dbJate = await openDB("jate", 1);
+
+  const trans = dbJate.transaction("jate", "readonly");
+
+  const jateStore = trans.objectStore("jate");
+
+  const request = jateStore.get(1);
+
+  const response = await request;
+  console.log(response);
+};
 
 initdb();
